6079a07c0f23e6b9982101a564932be7eb73680a
Luis <lfernapalacio@gmail.com>
2018-07-24 13:09:41 +0200

    Update AlumnosApplication.java

--- src/main/java/com/curso/alumnos/AlumnosApplication.java
+++ src/main/java/com/curso/alumnos/AlumnosApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AlumnosApplication {
 
 	public static void main(String[] args) {
-		//inicio
+		//inicio test
 		SpringApplication.run(AlumnosApplication.class, args);
 		//fin
 	}
